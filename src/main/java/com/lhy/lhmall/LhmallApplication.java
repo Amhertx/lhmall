ff8508afa0d6360f9e63c8b64533058e4605ab93
amhertx <491528520@qq.com>
2020-05-18 09:22:20 +0800

    初始化

---
+++ src/main/java/com/lhy/lhmall/LhmallApplication.java
@@ -0,0 +1,13 @@
+package com.lhy.lhmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LhmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LhmallApplication.class, args);
+    }
+
+}
